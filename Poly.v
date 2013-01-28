eaed8d1fb42bab9bea5b0c83fa0ff387a052d6c8
Ayaka Nonaka <ayanonagon@gmail.com>
2013-01-28 18:33:59 -0500

    solved flat_map

--- Poly.v
+++ Poly.v
@@ -849,12 +849,15 @@ Proof.
 
 Fixpoint flat_map {X Y:Type} (f:X -> list Y) (l:list X)
                    : (list Y) :=
-  (* FILL IN HERE *) admit.
+  match l with
+  | [] => []
+  | h :: t => f h ++ (flat_map f t)
+  end.
 
 Example test_flat_map1:
   flat_map (fun n => [n,n,n]) [1,5,4]
   = [1, 1, 1, 5, 5, 5, 4, 4, 4].
- (* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity.
 (** [] *)
 
 (** Lists are not the only inductive type that we can write a
