7ffcd930462a7cc0f27536b88ac60464369282ce
Ayaka Nonaka <ayanonagon@gmail.com>
2013-02-04 21:00:56 -0500

    solved rev_exercise1

--- MoreCoq.v
+++ MoreCoq.v
@@ -107,7 +107,12 @@ Theorem rev_exercise1 : forall (l l' : list nat),
      l = rev l' ->
      l' = rev l.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros l l'.
+  intros H.
+  rewrite -> H.
+  symmetry.
+  apply rev_involutive.
+Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, optional (apply_rewrite) *)
