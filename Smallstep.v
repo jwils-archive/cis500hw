096e2178265a088437b053ab1ce687f0b2607f04
Ayaka Nonaka <ayanonagon@gmail.com>
2013-04-05 15:53:00 -0400

    Solves step_deterministic

--- Smallstep.v
+++ Smallstep.v
@@ -393,7 +393,30 @@ Tactic Notation "step_cases" tactic(first) ident(c) :=
 Theorem step_deterministic :
   deterministic step.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  unfold deterministic. intros x y1 y2 Hy1 Hy2.
+  generalize dependent y2.
+  step_cases (induction Hy1) Case; intros y2 Hy2.
+    Case "ST_PlusConstConst". step_cases (inversion Hy2) SCase.
+      SCase "ST_PlusConstConst". reflexivity.
+      SCase "ST_Plus1". inversion H2.
+      SCase "ST_Plus2". inversion H3.
+    Case "ST_Plus1". step_cases (inversion Hy2) SCase.
+      SCase "ST_PlusConstConst". rewrite <- H0 in Hy1. inversion Hy1.
+      SCase "ST_Plus1".
+        rewrite <- (IHHy1 t1'0).
+        reflexivity. assumption.
+      SCase "ST_Plus2".
+        rewrite <- H in Hy1.
+        rewrite <- H in H1.
+        subst.
+        inversion H1. subst.
+        inversion Hy1.
+    Case "ST_Plus2". step_cases (inversion Hy2) SCase.
+      SCase "ST_PlusConstConst". subst. inversion Hy1.
+      SCase "ST_Plus1". subst. inversion H. subst. inversion H3.
+      SCase "ST_Plus2".
+        rewrite <- (IHHy1 t2'0).
+        reflexivity. assumption. Qed.
 (** [] *)
 
 (* ########################################################### *)
